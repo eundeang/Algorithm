db46802b449910393777de1e0002e1f7753b4f9a
eundeang <2021112027@dgu.ac.kr>
2023-05-23 18:41:28 +0900

    프로그래머스 12937 문제 풀이 완료

---
+++ Algorithm/Programmers/12937.swift
@@ -0,0 +1,18 @@
+//
+//  12937.swift
+//  Algorithm
+//
+//  Created by 허은정 on 2023/05/23.
+//
+
+import Foundation
+
+func solution(_ num:Int) -> String {
+    if num%2 == 0 {
+        return "Even"
+    }
+    else {
+        return "Odd"
+    }
+}
+

96d50907c503b2a692d371ee79444ce31aea42cd
eundeang <2021112027@dgu.ac.kr>
2023-06-16 00:05:50 +0900

    12912 문제 풀기 reduce 함수에 대해 알아보기

---
+++ Algorithm/Programmers/12912.swift
@@ -0,0 +1,27 @@
+//
+//  12912.swift
+//  Algorithm
+//
+//  Created by 허은정 on 2023/06/16.
+//
+
+import Foundation
+
+func solution(_ a:Int, _ b:Int) -> Int64 {
+    var answer = 0
+    if a == b {
+        return Int64(a)
+    }
+   else if a < b {
+       for i in a ... b {
+        answer += i
+        }
+       return Int64(answer)
+    }
+    else {
+        for i in b ... a {
+        answer += i
+        }
+        return  Int64(answer)
+    }
+}

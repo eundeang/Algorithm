6ecbe1b72728ed935d907704d02cded16d74d673
eundeang <2021112027@dgu.ac.kr>
2023-06-17 01:54:36 +0900

    86051 문제 풀이 완료 reduce 공부하기!

---
+++ Algorithm/Programmers/86051.swift
@@ -0,0 +1,19 @@
+//
+//  86051.swift
+//  Algorithm
+//
+//  Created by 허은정 on 2023/06/17.
+//
+
+import Foundation
+
+func solution(_ numbers:[Int]) -> Int {
+    var result = 0
+    
+    for i in 0...9{
+        if !numbers.contains(i) {
+            result += i
+        }
+    }
+    return result
+}

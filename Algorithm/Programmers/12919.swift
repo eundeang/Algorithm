c9da70434849a278ee32062644353d315f70bcc0
eundeang <2021112027@dgu.ac.kr>
2023-06-16 22:37:37 +0900

    12919 완벽 그자체임 index(of:)가 deprecated라는 말이 있긴 한데 공식문서에 따로 적혀지지 않은걸 보니, 전에 deprecated 됐다가 다시 살아난듯? 정확한건 확인해야할듯

---
+++ Algorithm/Programmers/12919.swift
@@ -0,0 +1,10 @@
+//
+//  12919.swift
+//  Algorithm
+//
+//  Created by 허은정 on 2023/06/16.
+//
+
+import Foundation
+
+func solution(_ seoul:[String]) -> String { "김서방은 \(seoul.index(of: "Kim")!)에 있다" }

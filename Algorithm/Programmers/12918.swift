58b53372880b1a794d6acb114caeea6807a4ec57
eundeang <heoeunjeong123@naver.com>
2023-07-13 23:36:27 +0900

    12918

---
+++ Algorithm/Programmers/12918.swift
@@ -0,0 +1,10 @@
+//
+//  12918.swift
+//  Algorithm
+//
+//  Created by 허은정 on 2023/07/13.
+//
+
+import Foundation
+
+func solution(_ s:String) -> Bool { (s.count == 4 || s.count == 6) && Int(s) != nil }

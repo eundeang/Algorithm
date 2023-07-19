a189d699c40b228f324564ab40dd5efa857f108d
eundeang <heoeunjeong123@naver.com>
2023-07-19 19:25:49 +0900

    12950 문제 풀이 완료 다른 사람들 풀이를 보니 zip함수라는 새로운 함수 발견 공부해볼것

---
+++ Algorithm/Programmers/12950.swift
@@ -0,0 +1,19 @@
+//
+//  12950.swift
+//  Algorithm
+//
+//  Created by 허은정 on 2023/07/19.
+//
+
+import Foundation
+
+func solution(_ arr1:[[Int]], _ arr2:[[Int]]) -> [[Int]] {
+    var result : [[Int]] = []
+    for i in 0..<arr1.count {
+        result.append([])
+        for j in 0..<arr1[i].count {
+            result[i].append(arr1[i][j] + arr2[i][j])
+        }
+    }
+    return result
+}

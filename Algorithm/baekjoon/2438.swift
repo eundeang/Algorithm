74569503c74b8614eb26ec087d65402e63fbbea1
eundeang <2021112027@dgu.ac.kr>
2023-10-10 23:02:00 +0900

    2438 문제 풀이 완료

---
+++ Algorithm/baekjoon/2438.swift
@@ -0,0 +1,14 @@
+//
+//  2438.swift
+//  Algorithm
+//
+//  Created by 허은정 on 2023/10/10.
+//
+
+import Foundation
+
+let n = Int(readLine()!)!
+
+for i in 1...n{
+    print(String(repeating: "*", count: i))
+}
